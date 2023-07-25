5bef6f704e6c5d1085a08ba5c980032e0758ab9f
Jack A <jackaddison3@live.com>
2023-07-25 12:40:00 -0400

    server fix

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/proxy', (req, res) => {
     encoding: null, // This ensures that the body is returned as a Buffer
   };
 
-  request({url: url, headers: {'User-Agent': 'request'}}, (error, response, body) => {
+  request(options, (error, response, body) => {
     if (error) {
       return res.status(500).json({type: 'error', message: error.message});
     }
@@ -27,9 +27,18 @@ app.get('/proxy', (req, res) => {
       return res.status(response.statusCode).json({type: 'error', message: 'Unexpected status code: ' + response.statusCode});
     }
 
-    res.json(JSON.parse(body));
-});
+    // Set the correct content type
+    res.set('Content-Type', response.headers['content-type']);
 
+    // Check if the response is JSON
+    if (response.headers['content-type'] === 'application/json') {
+      // If it's JSON, parse it and send it as JSON
+      res.json(JSON.parse(body));
+    } else {
+      // If it's not JSON, send it as is
+      res.send(body);
+    }
+  });
 });
 
 const PORT = process.env.PORT || 3000;
